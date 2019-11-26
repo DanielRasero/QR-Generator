348fb589bae8068017cdb1440a3c0951dd0952c2
Daniel Rasero Marín <danielraseromarin@gmail.com>
2019-11-26 11:58:24 +0100

    -Creación VentanaPrincipal

---
+++ src/main/java/com/mycompany/qrgenerator/VentanaPrincipal.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.qrgenerator;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author daniel
+ */
+
+//Clase que hereda de JFrame
+public class VentanaPrincipal extends JFrame {
+ 
+    //Tamaño de la ventana
+    
+    
+}
