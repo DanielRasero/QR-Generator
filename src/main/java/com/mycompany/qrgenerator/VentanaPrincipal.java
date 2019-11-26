264fd0ac8b1cfd2eb896251ed732dfe0935b70a5
Daniel Rasero Marín <danielraseromarin@gmail.com>
2019-11-26 12:15:12 +0100

    -Creamos posicion y tamaño, título y visibilidad de la ventana, opción de cerrado y que la ventana no sea redimensionable

--- src/main/java/com/mycompany/qrgenerator/VentanaPrincipal.java
+++ src/main/java/com/mycompany/qrgenerator/VentanaPrincipal.java
@@ -19,4 +19,28 @@ public class VentanaPrincipal extends JFrame {
     public static final int ANCHO_FRAME= 600;
     public static final int ALTO_FRAME= 200;
     
+    public VentanaPrincipal(){
+        
+        crearVentana();
+        
+    }
+    
+    private void crearVentana(){
+        
+        //posición y tamaño de la ventana
+        this.setBounds(600, 150, ANCHO_FRAME, ALTO_FRAME);
+        
+        //Título para la ventana
+        this.setTitle("Generador de códigos QR");
+        
+        //Ventana no redimensionable
+        this.setResizable(false);
+        
+        //Cerrar programa al pulsar es aspa
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        //Hacer visible la ventana
+        this.setVisible(true);
+    }
+    
 }
