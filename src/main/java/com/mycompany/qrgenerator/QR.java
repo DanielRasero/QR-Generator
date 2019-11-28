7e1e63e4d92d13272da9916925cc9119e594faf7
Daniel Rasero Marín <danielraseromarin@gmail.com>
2019-11-28 11:00:55 +0100

    -Creación de clase QR

---
+++ src/main/java/com/mycompany/qrgenerator/QR.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.qrgenerator;
+
+/**
+ *
+ * @author daniel
+ */
+public class QR {
+
+public static final int ANCHO_QR = 400;
+public static final int ALTO_QR = 400;
+    
+}
