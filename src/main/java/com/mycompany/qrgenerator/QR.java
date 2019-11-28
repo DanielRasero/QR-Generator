134c329f90d65053875d572f0859eb61ea1240f4
Daniel Rasero Marín <danielraseromarin@gmail.com>
2019-11-28 11:51:48 +0100

    -Modificaciones en clase QR

--- src/main/java/com/mycompany/qrgenerator/QR.java
+++ src/main/java/com/mycompany/qrgenerator/QR.java
@@ -13,5 +13,24 @@ public class QR {
 
 public static final int ANCHO_QR = 400;
 public static final int ALTO_QR = 400;
+
+//Este método genera un archivo en el disco duro con el nombre que se indica
+//en la variable "fichero". El texto que codifica está contenido en "texto"
+//y el formato de la imagen puede ser jpeg, tiff, gif, png.
+public static void escribirQR(String texto, String fichero, String formatoImagen){
     
+    //Objeto QRCodeWriter
+    //Renderiza un código QR como una matriz de dos dimensiones (BitMatrix)
+    
+    QRCodeWriter qrcw= new QRCodeWriter();
+    BitMatrix matrizPuntos= null;
+    
+    try{
+    matrizPUntos= qrcw.encode(texto, BarcodeFormat.QR_CODE, ALTO_QR, ALTO_QR);
+    
+    }catch(WriterException ex){
+        
+        
+    }
+}
 }
